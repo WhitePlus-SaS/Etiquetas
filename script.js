195d9ae0bfadb308e26ba266c5f4ea72359b09c4
White <85571401+WhitePlus-SaS@users.noreply.github.com>
2021-08-23 19:34:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal();
+
+  sr.reveal(".uno", {
+    duration: 4000,
+    origin: "rigth",
+    distance: "-100px"
+  })
+  sr.reveal(".tres", {
+    duration: 4000,
+    origin: "rigth",
+    distance: "-100px"
+  })
+  sr.reveal(".five", {
+    duration: 4000,
+    origin: "rigth",
+    distance: "-100px"
+  })
+  sr.reveal(".dos", {
+    duration: 4000,
+    origin: "left",
+    distance: "-100px"
+  })
+  sr.reveal(".cuatro", {
+    duration: 4000,
+    origin: "left",
+    distance: "-100px"
+  })
+  sr.reveal(".princ", {
+    duration: 4000
+  })
+  sr.reveal("header", {
+    duration: 4000,
+    origin: "left",
+    distance: "-300px"
+  })
